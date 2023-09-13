755f81e56bdce6a948e06aa86a6e6adb868b13d2
0x9F <30807136+0x9F1337@users.noreply.github.com>
2023-09-13 19:51:30 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ExampleTemplate
+{
+    internal class Program
+    {
+        const string GUID = "$guid";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine( "Running from ExampleTemplate with GUID {0}", GUID );
+        }
+    }
+}
